e9e30e8527fa440bfb66afe2ae1c32a508a818df
Denis Ergin <denis.ergin@diva-e.com>
2023-05-01 16:04:41 +0200

    feat: first implementation with axum
    to learn

---
+++ src/main.rs
@@ -0,0 +1,86 @@
+use axum::{
+    Router,
+    extract::State,
+    extract::Path,
+    routing::get,
+    http::StatusCode,
+    Json,
+};
+use serde::{Serialize};
+use std::{sync::{Mutex, Arc}};
+
+#[derive(Clone)]
+struct AppState {
+    current_count: Arc<Mutex<i32>>,
+    user_list: Arc<Mutex<Vec<User>>>,
+}
+
+#[derive(Clone, Serialize)]
+struct User {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize)]
+struct UserListResponse {
+    users: Vec<User>
+}
+
+impl User {
+    fn new(username: String, password: String) -> User {
+        User {
+            username,
+            password,
+        }
+    }
+}
+
+async fn get_state  (
+    State(state): State<AppState>,
+) -> (StatusCode, String) {
+    let count = state.current_count.lock().unwrap();
+    (StatusCode::OK, count.to_string())
+}
+
+async fn add_to_state (
+    State(state): State<AppState>,
+    Path(value): Path<i32>,
+) -> (StatusCode, String) {
+    let mut count = state.current_count.lock().unwrap();
+    *count += value;
+    (StatusCode::OK, ("Added ".to_string() + &value.to_string() + &" to count!".to_string()))
+}
+
+fn get_default_user () -> Vec<User> {
+    vec![User::new("denis".to_string(), "asdf1234".to_string())]
+}
+
+async fn get_all_users (
+    State(state): State<AppState>
+) -> (StatusCode, Json<UserListResponse>) {
+    let users = state.user_list.lock().unwrap();
+    let response = UserListResponse {
+        users: users.to_vec()
+    };
+    (StatusCode::OK, Json(response))
+}
+
+#[tokio::main]
+async fn main() {
+    let state = AppState {
+        current_count: Arc::new(Mutex::new(0)),
+        user_list: Arc::new(Mutex::new(get_default_user()))
+    };
+
+    let app = Router::new()
+        .route("/", get(get_state))
+        .route("/add/:value", get(add_to_state))
+        .route("/users", get(get_all_users))
+        .with_state(state);
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"127.0.0.1:8080".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
