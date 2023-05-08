c3bb452a2de4a4bfa7d1041bd8fe57a17e326874
Denis Ergin <denis.ergin@diva-e.com>
2023-05-08 18:31:55 +0200

    simplify get_authorization_as_uuid

--- src/utils/parser.rs
+++ src/utils/parser.rs
@@ -1,16 +1,12 @@
-use axum::http::HeaderMap;
+use axum::http::{HeaderMap, header};
 use uuid::Uuid;
 
-pub fn get_authorization_as_uuid(headers: &HeaderMap) -> Result<Uuid, String> {
-  let auth_header_value = headers.get("Authorization");
-  if auth_header_value.is_none() {
-    return Err("Authorization-Header is not set".to_string());
-  }
-  let auth_string_value = auth_header_value.unwrap().to_str().ok().unwrap();
-  let auth_uuid = Uuid::parse_str(auth_string_value);
-  if auth_uuid.is_ok() {
-    Ok(auth_uuid.ok().unwrap())
-  } else {
-    Err("Failed to parse Authorziation Header".to_string())
-  }
+pub fn get_authorization_as_uuid(headers: &HeaderMap) -> Option<Uuid> {
+  let auth_uuid = headers
+    .get(header::AUTHORIZATION)
+    .and_then(|header| header.to_str().ok())
+    .and_then(|auth_value| Uuid::parse_str(auth_value).ok());
+
+  auth_uuid
 }
+
