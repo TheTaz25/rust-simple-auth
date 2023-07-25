aee6568fd24d5048f1eed88fc6f834c425e77d62
Denis Ergin <denis.ergin@diva-e.com>
2023-07-25 09:04:58 +0200

    feat: admin guard check

--- src/middleware/authorized.rs
+++ src/middleware/authorized.rs
@@ -21,3 +21,28 @@ pub async fn logged_in_guard<B>(
     Err(Fault::NotLoggedIn)
   }
 }
+
+pub async fn admin_guard<B>(
+  State(state): State<AppState>,
+  mut req: Request<B>,
+  next: Next<B>,
+) -> Result<Response, Fault> {
+  let auth_token = get_authorization_as_uuid(&req.headers());
+  if let Ok(auth_token) = auth_token {
+    let user_uuid = state.redis.get_user_for_access_token(&auth_token).await?;
+
+    let mut connection = state.pool.get_connection().await?;
+
+    let user = q_get_user_by_id(&mut connection.connection, user_uuid).await?;
+
+    if let Some(admin) = user.admin {
+      if admin {
+        req.extensions_mut().insert(user);
+        return Ok(next.run(req).await);
+      }
+    }
+    return Err(Fault::Unallowed)
+  } else {
+    Err(Fault::NotLoggedIn)
+  }
+}
